90a8b64a761510bc7e86a51800df122977bdbc24
juaruifra <juaruifra@alu.edu.gva.es>
2026-01-14 18:27:35 +0100

    Dejar todo como estaba, de momento

--- index_OLD.php
+++
@@ -1,8 +0,0 @@
-<?php
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-if ($uri === '/storage') {
-    require 'storage.php';
-    exit;
-}
-echo "Hello World! Juan Antonio Ruiz Francés";
